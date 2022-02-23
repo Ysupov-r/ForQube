f48cc664443feb8db21db66a6f0f8a19a759f665
rashid <rashid-11-26@mail.ru>
2022-02-23 11:41:52 +0600

    add question

--- src/app/Pages/admin/admin.component.ts
+++ src/app/Pages/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { TestService, UserResult } from 'src/app/modules/shared/test.service';
 
 @Component({
@@ -12,15 +14,34 @@ export class AdminComponent implements OnInit {
   isTestChanges: boolean = true;
   resultList: UserResult[] = [];
 
+  form = new FormGroup({
+    Question: new FormControl(null, Validators.required),
+    TrueAnswer: new FormControl(null, Validators.required),
+    AnswerOpt: new FormControl(null, Validators.required),
+  });
+
   constructor(
     private service: TestService,
-    private router: Router
+    private router: Router,
+    private toast: ToastrService
   ) {
     this.resultList = this.service.resultList;
   }
 
   ngOnInit(): void {
     console.log('resList', this.resultList);
+  }
+
+  onSubmit(): void {
+    const obj = {
+      id: this.service.animalTest.length + 1,
+      question: this.form.value.Question,
+      answerOpt: this.form.value.AnswerOpt.trim().split(' '),
+      trueAnswer: this.form.value.TrueAnswer,
+      answer: ''
+    }
+    this.service.animalTest.push(obj);
+    this.toast.success('Вопрос был добавлен');
   }
 
   selectItem(item: string): void {
